26f1165b5fd4bda8d4a2d323fbca3d4b63dc1a3b
Dan Kelleher <dkelleher@g.harvard.edu>
2017-11-01 15:54:55 -0400

    Add pre/post activation/deactivation hooks
    The pre and post, activation and deactivation hooks give people the
    ability to run scripts before and after activation or deactivation.  The
    default behavior is to prepend the environment name to the front of the
    prompt.  This idea was taken from the python virtualenvwrapper project.
    
    Also i moved the tab completion function up in the file.  I think it
    makes more sense organized this way.

--- go-env-wrapper.zsh
+++ go-env-wrapper.zsh
@@ -1,4 +1,6 @@
-
+#
+#
+#
 
 if [[ -z $GOENVPATH ]]; then
 	echo "GOENVPATH not set"
@@ -14,6 +16,54 @@ function _goenv-envpath {
 function _goenv-help-string {
 	echo "Usage: $0 {create|activate|deactivate|delete} name" >&2
 	exit 1
+}
+
+
+function _go-env-pre-activate-hook-tmpl {
+	cat > $1 <<-EOT
+		#!/usr/bin/env zsh
+		export PREGOENVPS=\$PS1
+		EOT
+}
+
+
+function _go-env-post-activate-hook-tmpl {
+	cat > $1 <<-EOT
+		#!/usr/bin/env zsh
+		export PS1="(\$1)::\$PS1"
+		EOT
+}
+
+
+function _go-env-pre-deactivate-hook-tmpl {
+	cat > $1 <<-EOT
+		#!/usr/bin/env zsh
+		EOT
+}
+
+
+function _go-env-post-deactivate-hook-tmpl {
+	cat > $1 <<-EOT
+		#!/usr/bin/env zsh
+		export PS1=\$PREGOENVPS
+		unset PREGOENVPS
+		EOT
+}
+
+
+function _go-env-pre-post-hook-generator {
+	if [[ ! -a $GOENVPATH/preactivate ]]; then
+		_go-env-pre-activate-hook-tmpl $GOENVPATH/preactivate
+	fi
+	if [[ ! -a $GOENVPATH/postactivate ]]; then
+		_go-env-post-activate-hook-tmpl $GOENVPATH/postactivate
+	fi
+	if [[ ! -a $GOENVPATH/predeactivate ]]; then
+		_go-env-pre-deactivate-hook-tmpl $GOENVPATH/predeactivate
+	fi
+	if [[ ! -a $GOENVPATH/postdeactivate ]]; then
+		_go-env-post-deactivate-hook-tmpl $GOENVPATH/postdeactivate
+	fi
 }
 
 
@@ -36,9 +86,11 @@ function _goenv-activate {
 
 	if [[ -d $env_path ]]; then
 		echo "===> Activating go-env $1"
+		source $GOENVPATH/preactivate
 		export GOPATH=$env_path
 		export PATH=$env_path/bin:$PATH
 		export GOENV_ACTIVATED=$1
+		source $GOENVPATH/postactivate
 		return 0
 	else
 		echo "===> Failure to activate $1, may not exist."
@@ -50,9 +102,11 @@ function _goenv-activate {
 function _goenv-deactivate {
 	if [[ -v GOENV_ACTIVATED ]]; then
 		echo "===> Deactivation go-env $1"
+		source $GOENVPATH/predeactivate
 		export PATH=$(echo $PATH | sed "s|$GOPATH/bin:||g")
 		unset GOPATH
 		unset GOENV_ACTIVATED
+		source $GOENVPATH/postdeactivate
 		return 0
 	else
 		echo "===> Failure to deactivate $1, environment may not be active."
@@ -79,6 +133,16 @@ function _goenv-delete {
 		echo "===> Failure to delete $1, environment may not exist."
 		return 1
 	fi
+}
+
+
+function _go-env() {
+
+	_arguments -C \
+		"-h[Show help information]" \
+		"--h[Show help information]" \
+		"1:go-env command:(create activate deactivate delete)" \
+		"2:go-env environments:($GOENVPATH/envs/*(:t))"
 }
 
 
@@ -109,14 +173,7 @@ function go-env {
 }
 
 
-function _go-env() {
-
-	_arguments -C \
-		"-h[Show help information]" \
-		"--h[Show help information]" \
-		"1:go-env command:(create activate deactivate delete)" \
-		"2:go-env environments:($GOENVPATH/envs/*(:t))"
-}
+_go-env-pre-post-hook-generator
 
 
 compdef _go-env go-env
